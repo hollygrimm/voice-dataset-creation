c91fcf2b376b1219cb2ff37b5a1c58a79a809dda
Holly Grimm <grimmh@google.com>
2020-06-26 09:12:10 -0600

    add test data

--- scripts/wav_to_text.py
+++ scripts/wav_to_text.py
@@ -11,10 +11,10 @@ from tqdm import tqdm
 from google.cloud import speech_v1p1beta1
 from google.cloud.speech_v1p1beta1 import enums
 
-def audition(input_filename, output_filename, language_code, sample_rate_hertz):
-    stt(input_filename, output_filename, language_code, sample_rate_hertz)
+def audition(wavs_path, input_filename, output_filename, language_code, sample_rate_hertz):
+    stt(wavs_path, input_filename, output_filename, language_code, sample_rate_hertz)
 
-def stt(input_filename, output_filename, language_code, sample_rate_hertz):
+def stt(wavs_path, input_filename, output_filename, language_code, sample_rate_hertz):
     client = speech_v1p1beta1.SpeechClient()
     encoding = enums.RecognitionConfig.AudioEncoding.MP3
     config = {
@@ -27,7 +27,7 @@ def stt(input_filename, output_filename, language_code, sample_rate_hertz):
 
     df = pd.read_csv(input_filename, sep='\t', encoding='utf-8')
     for wav_marker, start, duration, time_format, marker_type in zip(df['Name'].to_list(), df['Start'].to_list(), df['Duration'].to_list(), df['Time Format'].to_list(), df['Type'].to_list()):
-        local_file_path = f'wavs/{wav_marker}.wav'
+        local_file_path = f'{wavs_path}/{wav_marker}.wav'
         with io.open(local_file_path, "rb") as f:
             content = f.read()
         audio = {"content": content}
@@ -64,8 +64,9 @@ def execute_cmdline(argv):
         return subparsers.add_parser(cmd, description=desc, help=desc, epilog=epilog)
 
     p = add_command(    'audition',         'Audition format', 'audition')
-    p.add_argument(     '--input_filename',  default="Markers.csv")
-    p.add_argument(     '--output_filename', default="Markers_STT.csv")
+    p.add_argument(     '--wavs_path',  default="../test_data/wavs")
+    p.add_argument(     '--input_filename',  default="../test_data/Markers.csv")
+    p.add_argument(     '--output_filename', default="../test_data/Markers_STT.csv")
     p.add_argument(     '--sample_rate_hertz', default=48000)
     p.add_argument(     '--language_code', default='en-US')
 
